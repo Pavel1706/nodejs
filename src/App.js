c0d4a6f770d4b1a9d4c64d8a66fa5f1df906d341
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-21 14:18:16 +0300

    add feature 'addUser'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React, {useEffect} from 'react';
-
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+   const[users, setUsers]= useState([]);
+
+   const getUsers = ()=>{
+       axios.get('http://localhost:7542/users').then(res=>{
+           setUsers(res.data)
+       });
+   }
+
+
     useEffect(() => {
+        getUsers()
+    }, []);
 
-    }, [])
-    return (
+
+    const createUser = ()=>{
+        axios.post("http://localhost:7542/users").then(res => {
+           getUsers();
+        })
+    }
+
+    return (<>
         <div>
-
+            <button onClick={createUser}>create user</button>
         </div>
+        <div>
+            {users.map(u => <div key={u.id}>{u.name}</div>)}
+        </div>
+        </>
     );
 }
 
